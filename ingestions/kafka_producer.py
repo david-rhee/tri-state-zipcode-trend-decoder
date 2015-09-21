907c00b8cbdb0290d64facf37bac38b44b1f9464
drhee1 <drhee1@drhee1s-MacBook-Pro.local>
2015-09-21 13:32:09 -0400

    Add ingestion codes

---
+++ ingestions/kafka_producer.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+import os, sys, six, random, time
+
+from datetime import datetime
+from kafka.client import KafkaClient
+from kafka.producer import KeyedProducer
+
+from kafka_producer_utils import parse_prefilter_data, build_distribution_based_zipcode_list, build_random_zipcode_list, emit_random_zipcode_price
+
+class Producer(object):
+
+    def __init__(self, addr):
+        self.client = KafkaClient(addr)
+        self.producer = KeyedProducer(self.client)
+
+    def produce_house_centric_msgs(self, source_symbol):
+        # Declare variables
+        timing_list = []
+    
+        # Read throughput timing distribution
+        with open('data/throughput/throughput_timing.txt', 'rU') as infile:
+            for line in infile :
+                timing_list.append(line.strip())
+    
+        # 0 to 86
+        for x in range(0,86):        
+            zipcode_sales_dict = parse_prefilter_data('data/tri_zipcode_sales/%s.txt'%x)
+            zipcode_price_dict = parse_prefilter_data('data/tri_zipcode_price/%s.txt'%x)
+        
+            random_zipcode_sales_list = build_random_zipcode_list(zipcode_sales_dict)
+            distribution_zipcode_sales_list = build_distribution_based_zipcode_list(zipcode_sales_dict)
+    
+            t_end = time.time() + 60 * 60 # every hour
+    
+            while time.time() < t_end:
+                time_field = datetime.now().strftime("%Y%m%d-%H%M%S")
+                user_id_field = 0
+                random_number = random.randint(0, (len(random_zipcode_sales_list)-1)) 
+                user_zipcode = random_zipcode_sales_list[random_number]
+                house_field = emit_random_zipcode_price(distribution_zipcode_sales_list, zipcode_price_dict)
+                str_fmt = """{{"timestamp":"{}","user":{{"id":"{}","zipcode":"{}"}},"house":{}}}"""
+                message_info = str_fmt.format(time_field, user_id_field, user_zipcode, house_field)
+        
+                print message_info
+                self.producer.send_messages('test_data', source_symbol, message_info)
+    
+                if float(timing_list[x]) != 0:
+                    time.sleep(float(timing_list[x]))
+
+    def produce_user_centric_msgs(self, source_symbol):
+        user_id_list = ('1', '2')
+        user_zipcode_list = ('10461', '07304')
+        house_zipcode_list = ('10545', '07304')
+        price_list = ('315789', '299679')
+
+        for i in range(0, 2):    
+            t_end = time.time() + 60 * 1440 # for 24 hours
+    
+            while time.time() < t_end:
+                time_field = datetime.now().strftime("%Y%m%d-%H%M%S")
+                user_id_field = user_id_list[i]
+                user_zipcode = user_zipcode_list[i]
+                another_random_number = random.randint(int(float(price_list[i])-(float(price_list[i])*0.3)), int(float(price_list[i])+(float(price_list[i])*0.5)))
+                house_field = '{"zipcode":"{%s}","price":"{%s}"}'%(house_zipcode_list[i], another_random_number)
+                str_fmt = """{{"timestamp":"{}","user":{{"id":"{}","zipcode":"{}"}},"house":{}}}"""
+                message_info = str_fmt.format(time_field, user_id_field, user_zipcode, house_field)
+        
+                print message_info
+                self.producer.send_messages('test_data', source_symbol, message_info)
+    
+                time.sleep(600) # send message every 10 minutes
+
+if __name__ == "__main__":
+
+    args = sys.argv
+    ip_addr = str(args[1])
+    partition_key = str(args[2])
+    option = str(args[3])
+
+    prod = Producer(ip_addr)
+    if option == 'house':
+        prod.produce_house_centric_msgs(partition_key)
+    elif option == 'user':
+        prod.produce_user_centric_msgs(partition_key)
---
+++ ingestions/kafka_producer_utils.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+import os, string, random, time
+from datetime import datetime
+
+''' 
+
+PURPOSE: Utility functions for generating fake user activities.
+DATE:
+Version:
+AUTHOR: David Rhee
+Modifier:
+
+Parses the Home Sales list from http://www.zillow.com/research/data/ for the zipcode and number of houses sold
+per month. The number of houses sold per month is used to increase the likelihood of zipcode with a higher houses
+sold to be chosen during random sampling.
+
+'''
+
+########################################################################################################################
+########################################################################################################################
+# Pre-filetering Functions
+######################################################################################################################## 
+def prefilter_sales_zip_tri():
+    """
+    Pre-filter Sales_Zip_Tri.txt
+    """
+    # Declare variables
+    line_count = 0
+    sales_zip_dict = {}
+    
+    # Read line by line
+    with open('data/original/Sales_Zip_Tri.txt', 'rU') as infile:
+        for line in infile :
+            if not line_count == 0 : # skip first line
+                tmp_list = line.strip().split("\t") # split line into a list
+                key = tmp_list[0]
+                del tmp_list[0:2]
+                sales_zip_dict[key] = tmp_list # store list into a dictionary
+    
+            line_count += 1
+    
+    keys = sales_dict.keys()
+    keys.sort()
+    
+    #86 columns
+    for x in range(0, 86):
+        output_filename = 'data/tri_zipcode_sales/%s.txt'%x
+    
+        outfile = open(output_filename,'w')
+        for key in keys:    
+            outfile.write("%s\t%s\n"%(key,str(int(sales_dict[key][x])+1)))
+        outfile.close()
+
+def prefilter_zhvi_all_homes_zip():
+    """
+    Pre-filter Zip_Zhvi_AllHomes_Tri.txt
+    """
+
+    # Declare variables
+    line_count = 0
+    zhvi_all_homes_zip_dict = {}
+
+    # Read line by line
+    with open('data/original/Zip_Zhvi_AllHomes_Tri.txt', 'rU') as infile:
+        for line in infile :
+            if not line_count == 0 : # skip first line
+                tmp_list = line.strip().split("\t") # split line into a list
+                key = tmp_list[0]
+                del tmp_list[0:4]
+                zhvi_all_homes_zip_dict[key] = tmp_list # store list into a dictionary
+
+            line_count += 1
+
+    keys = zhvi_all_homes_zip_dict.keys()
+    keys.sort()
+    
+    #86 columns
+    for x in range(0, 86):
+        output_filename = 'data/tri_zipcode_price/%s.txt'%x
+    
+        outfile = open(output_filename,'w')
+        for key in keys:    
+            outfile.write("%s\t%s\n"%(key,zhvi_all_homes_zip_dict[key][x]))
+        outfile.close()
+
+########################################################################################################################
+# Utility Functions
+########################################################################################################################
+def parse_prefilter_data(filename):
+    """
+    Parses pre-filtered *.txt files
+    """
+
+    # Declare variables
+    tmp_dict = {}
+
+    # Read line by line
+    with open(filename, 'rU') as infile:
+        for line in infile :
+            tmp_list = line.strip().split("\t") # split line into a list
+            tmp_dict[tmp_list[0]] = tmp_list[1]
+
+    return tmp_dict
+
+def build_distribution_based_zipcode_list(sales_zip_dict):
+    """
+    
+    """
+
+    # Declare variables
+    distribution_zipcode_list = []
+    
+    keys = sales_zip_dict.keys()
+    keys.sort()
+
+    for key in keys:
+        for x in range(0, int(sales_zip_dict[key])):
+            distribution_zipcode_list.append(key)
+
+    return distribution_zipcode_list
+
+def build_random_zipcode_list(sales_zip_dict):
+    """
+    
+    """
+
+    # Declare variables
+    random_zipcode_list = []
+    
+    keys = sales_zip_dict.keys()
+    keys.sort()
+
+    for key in keys:
+        random_zipcode_list.append(key)
+
+    return random_zipcode_list
+
+def emit_random_zipcode_price(built_zipcode_list, zhvi_all_homes_zip_dict):
+    """
+    
+    """
+
+    random_number = random.randint(0, (len(built_zipcode_list)-1)) 
+    zipcode = built_zipcode_list[random_number]
+    tmp_price = zhvi_all_homes_zip_dict[str(zipcode)]
+
+    another_random_number = random.randint(int(float(tmp_price)-(float(tmp_price)*0.3)), int(float(tmp_price)+(float(tmp_price)*0.5)))
+
+    str_fmt = """{{"zipcode":"{}","price":"{}"}}"""
+    message_info = str_fmt.format(zipcode, another_random_number)
+
+    return message_info
